7144397ab46ca15b164f86dc3be69aff4f43d8b4
dannhanong <thannong0512@gmail.com>
2025-02-16 21:43:03 +0700

    update socket

--- src/main/java/com/dan/wedding/services/impls/WishServiceImpl.java
+++ src/main/java/com/dan/wedding/services/impls/WishServiceImpl.java
@@ -5,6 +5,7 @@ import com.dan.wedding.models.Wish;
 import com.dan.wedding.repositories.WishRepository;
 import com.dan.wedding.services.WishService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +14,14 @@ import java.util.List;
 public class WishServiceImpl implements WishService {
     @Autowired
     private WishRepository wishRepository;
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     @Override
     public Wish create(Wish wish) {
-        return wishRepository.save(wish);
+        Wish w = wishRepository.save(wish);
+        simpMessagingTemplate.convertAndSend("/topic/wishes", w);
+        return w;
     }
 
     @Override
